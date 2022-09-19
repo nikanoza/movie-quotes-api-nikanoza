01ba604499809bda6477aab41a66728a9f0adc2d
nikanoza <nikanozadze1010@gmail.com>
2022-09-19 08:55:51 +0400

    feat: create send password recovery route and add it to swagger

--- src/routes/user-router.ts
+++ src/routes/user-router.ts
@@ -1,9 +1,10 @@
-import { emailVerification, registration } from 'controllers'
+import { emailVerification, passwordRecoverySend, registration } from 'controllers'
 import express from 'express'
 
 const userRouter = express.Router()
 
 userRouter.post('/register', registration)
 userRouter.post('/verify', emailVerification)
+userRouter.post('/password/send-link', passwordRecoverySend)
 
 export default userRouter
